2a126ff578b9874e12852a36cd63a45715a64c94
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-10 04:33:56 +0600

    initial commit

---
+++ variable.js
@@ -0,0 +1,16 @@
+// String
+var name = "Sourav";
+var address = "Rajshahi";
+var country = 'Bangladesh';
+
+// boolean
+var isRich = false;
+
+var passed = true;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+// number
+var price = 599;
+console.log(typeof price);
